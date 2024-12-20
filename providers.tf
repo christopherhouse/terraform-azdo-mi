6dec21fadc4152c8dfc097c4fafb72c754205a73
Chris House <chhouse@microsoft.com>
2024-12-20 12:58:05 -0600

    updated config

--- providers.tf
+++ providers.tf
@@ -26,8 +26,8 @@ terraform {
 provider "azurerm" {
   features {}
 
-  use_msi = true
-  subscription_id = "8bd05b2f-62c5-4def-9869-f0617ebb3970"
-  client_id = "f4c5cd5b-3a07-427d-94ed-848501abda7b"
-  tenant_id = "76de2d2d-77f8-438d-9a87-01806f2345da"
+  # use_msi = true
+  # subscription_id = "8bd05b2f-62c5-4def-9869-f0617ebb3970"
+  # client_id = "f4c5cd5b-3a07-427d-94ed-848501abda7b"
+  # tenant_id = "76de2d2d-77f8-438d-9a87-01806f2345da"
 }
