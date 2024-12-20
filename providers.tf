b7147a9a16124187529fe729594798993417400a
Chris House <chhouse@microsoft.com>
2024-12-20 11:27:12 -0600

    updated provider conf

--- providers.tf
+++ providers.tf
@@ -17,6 +17,6 @@ provider "azurerm" {
 
   use_msi = true
   subscription_id = "8bd05b2f-62c5-4def-9869-f0617ebb3970"
-  arm_client_id = "f4c5cd5b-3a07-427d-94ed-848501abda7b"
+  client_id = "f4c5cd5b-3a07-427d-94ed-848501abda7b"
   tenant_id = "76de2d2d-77f8-438d-9a87-01806f2345da"
 }
