0965f066e217903ead96a9351a2be8756eaa9122
Chris House <chhouse@microsoft.com>
2024-12-20 11:37:44 -0600

    updated backend config

--- providers.tf
+++ providers.tf
@@ -10,6 +10,17 @@ terraform {
         version = "~>3.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name    = "rg-tf"
+    storage_account_name   = "cmhtfstatesa"
+    container_name         = "tfstate"
+    key                    = "terraform.tfstate"
+    use_oidc = true
+    client_id = "f4c5cd5b-3a07-427d-94ed-848501abda7b"
+    subscription_id = "8bd05b2f-62c5-4def-9869-f0617ebb3970"
+    tenant_id = "76de2d2d-77f8-438d-9a87-01806f2345da"
+    use_azuread_auth = true
+  }
 }
 
 provider "azurerm" {
